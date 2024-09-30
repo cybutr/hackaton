ade727cd7f36fef98817a1dd4bf838f6e1546923
petrkapr <petrkrotky01@gmail.com>
2024-09-30 17:48:38 +0200

    pygame-hrazacate

--- hra.py
+++ hra.py
@@ -7,29 +7,28 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50))
+player = pygame.Rect(300, 250, 50, 50)
 
 run = True
 while run:
-    
     screen.fill((0, 0, 0))
-    
-    pygame.draw.rect(screen, (255, 0, 0), player)
 
-key = pygame.key.get_pressed()
-if key[pygame.K_a] == True:
-    player.move_ip(-1, 0)
-elif key[pygame.K_d] == True:
-    player.move_ip(1, 0)
-elif key[pygame.K_w] == True:
-    player.move_ip(0, -1)
-elif key[pygame.K_s] == True:
-    player.move_ip(0, 1)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        player.x -= 1
+    if keys[pygame.K_LEFT]:
+        player.x += 1
+    if keys[pygame.K_UP]:
+        player.y -= 1
+    if keys[pygame.K_DOWN]:
+        player.y += 1
+    
+    pygame.draw.rect(screen, (255, 128, 0), player)
     pygame.display.update()
 
 pygame.quit()
